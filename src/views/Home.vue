db497388649b175aec1827128ea93edc21ec7c15
Taddes Korris <taddes.korris@passportinc.com>
2019-05-04 09:34:26 -0400

    assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,34 @@
 <template>
  <v-app>
   <v-img :src="banner.src" alt="Skoolaide">
-    <v-content class="pa-2 white--text hidden-md-and-down" id="banner-content" >
+    <v-content app class="pa-2 white--text hidden-md-and-down" id="banner-content" >
         <h1>Guide and reward student success</h1>
         <p class="headline">SkoolAide helps you <strong>set</strong> goals and <strong>earn</strong><br> rewards for doing well in school!</p>
         <v-spacer></v-spacer>
         <v-btn large class="" color="success" href="https://www.skoolaide.com/#/start" >Get started</v-btn>
         <VideoModal />
       </v-content>
+      <br>
+      <br>
+      <br>
+     <v-content app class="pa-2 white--text hidden-md-and-down" id="tagline-content">
+        <h1>{{ tagLine.subheader }}</h1>
+      </v-content>
   </v-img>
 
-    <v-content class="pa-2 ma-2 hidden-lg-and-up" >
+    <v-content class="pa-2 ma-2 hidden-lg-and-up text-xs-center" >
+      <v-spacer></v-spacer>
+      <br>
       <h1>Guide and reward student success</h1>
       <p class="headline">SkoolAide helps you <strong>set</strong> goals and <strong>earn</strong> rewards for doing well in school!</p>
       <v-spacer></v-spacer>
       <v-btn large class="" color="success" href="https://www.skoolaide.com/#/start" >Get started</v-btn>
       <VideoModal />
     </v-content>
+
+      <v-content class="pa-2 ma-2 hidden-lg-and-up text-xs-center" >
+        <h1>{{ tagLine.subheader }}</h1>
+      </v-content>
  
     <v-layout
       align-center
@@ -55,6 +67,10 @@ export default {
   data () {
     return {
       youtubeLink: 'https://www.youtube.com/watch?v=dtFj96Zu_Kg',
+      tagLine: {
+        header: 'SkoolAide allows students to get rewarded for doing well in school.',
+        subheader: 'Set goals.  Reach them. Unlock the reward that you earned!',
+      },
       banner: {
         name: 'banner',
         src: require('../assets/images/banner.png')
@@ -62,24 +78,12 @@ export default {
       logo: require('../assets/logo/logo_white.png'),
       footer: [
         {
-          name: 'footer1',
-          src: require('../assets/images/footer1.png')
-        },
-        {
           name: 'footer2',
           src: require('../assets/images/footer2.png')
         },
         {
-          name: 'footer3',
-          src: require('../assets/images/footer3.png')
-        },
-        {
           name: 'footer4',
           src: require('../assets/images/footer4.png')
-        },
-        {
-          name: 'footer5',
-          src: require('../assets/images/footer5.png')
         },
         {
           name: 'footer6',
@@ -93,8 +97,11 @@ export default {
 
 <style>
 #banner-content {
-  margin-top: 260px;
-  margin-left: 70px;
+  margin-top: 170px;
+  margin-left: 50px;
+}
+#tagline-content {
+  margin-left: 50px;
 }
 
 </style>
